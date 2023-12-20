467858f87506b2461f7e765711fec0f84e876cf7
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2023-12-20 08:24:00 +0300

    array hali çözüldü
    Harmonik seriyi array yardımıyla çözdük

--- Harmonik.java
+++ Harmonik.java
@@ -1,8 +1,44 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Harmonik {
+
+
     public static void main(String[] args) {
+        int arrayVariableCount;
+        double sum = 0;
+
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Kaç değerli bir dizi gireceksin?:  ");
+        arrayVariableCount = input.nextInt();
+
+        double[] myArray = new double [arrayVariableCount];
+
+        for(int i = 0; i < arrayVariableCount;i++){
+            myArray[i] = 1D/(i+1);
+
+        }
+
+        for (double i : myArray
+             ) {
+
+            sum += i;
+
+        }
+        System.out.println("Harmonik seri:  ");
+        System.out.println("Dizimiz:   "+Arrays.toString(myArray));
+        System.out.print("Harmonik ortalaması: "+sum/arrayVariableCount);
+    }
+}
+
+
+
+
+
+
+
+
+/*          Bu eski hali
         System.out.print("Harmonik sayıyı ver:  ");
         int harmonik = input.nextInt();
         // harmonik değerlerin toplamını total değişkeninde toplayacağız
@@ -14,5 +50,5 @@ public class Harmonik {
         }
         System.out.println("'in Toplamı:  ");
         System.out.println(total);
-    }
-}
+
+ */
